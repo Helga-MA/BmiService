69130170fb1eaf17bd59e9b7bdb763851cb11154
Olga Stepanova <os20142020@gmail.com>
2021-08-30 01:31:38 +0200

    Correct commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 56.0;
-        double height = 1.67;
-        double bMI = service.calculate(weight, height);
+        double bMI = service.calculate(56.0, 1.67);
         System.out.println("bMI: " + bMI);
 
     }
