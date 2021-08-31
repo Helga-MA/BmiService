e71aae2b9a34e03b70ea9593087cfe0e41aa1d6c
Olga Stepanova <os20142020@gmail.com>
2021-09-01 01:18:50 +0200

    New commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
     public double calculate(double weight, double height) {
         double bMI = weight / (height * height);
-        System.out.println("bMI: " + bMI);
         return bMI;
     }
 
